7008d89f19eed032be31b8e5b620b3798d09bf4a
ypjun100 <ypjun100@gmail.com>
2026-02-24 23:25:47 +0900

    feat: 도커 컨테이너 PTY <-> 소켓 연동 작업

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // NOTE: WebSocket 어댑터 설정 (socket.io 사용)
+    app.useWebSocketAdapter(new IoAdapter(app));
+
     await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
